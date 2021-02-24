6b46fe4cf64a9bb1ea705933c04492f97eb5dc68
Ashraful Jannat <ajannattt@gmail.com>
2021-02-24 23:28:54 +0600

    first react component

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>,
+//     document.getElementById('root')
+// );
+class Clock extends React.PureComponent {
+    render() {
+        const { locale } = this.props;
+        return (
+            <h1 className="heading">
+                <span className="text">
+                    Hello {new Date().toLocaleTimeString({ locale })} {locale}
+                </span>
+            </h1>
+        );
+    }
+}
 
+ReactDOM.render(<Clock locale="bn-bd" />, document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
