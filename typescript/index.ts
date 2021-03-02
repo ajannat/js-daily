21fc0967118fff084e9bf0b16fcb2f2e3a9fda40
Ashraful Jannat <ajannattt@gmail.com>
2021-03-03 00:55:20 +0600

    added classes

--- typescript/index.ts
+++ typescript/index.ts
@@ -16,3 +16,20 @@ let myFunction: (x: string, y: number, z: boolean, user: {id: string, name: stri
 myFunction = (a: string, b: number, z:boolean, user: { id: string, name: string }) => {
     return '123';
 }
+
+// classes
+class Player{
+    name: string;
+    age: number;
+    male: boolean;
+
+    constructor(n: string, a: number, m: boolean){
+        this.name = n;
+        this.age = a;
+        this.male = m;
+    }
+}
+
+const pl = new Player('jannat', 25, true);
+const arr: Player[] = [];
+arr.push(pl);
